5c5a089fc0c0f4edc449371ecc7bb5f75569617c
AIToolzHub <aitoolzhub26@gmail.com>
2025-06-21 00:15:13 +0800

    Create SubmitPage.jsx

---
+++ src/pages/SubmitPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SubmitToolForm from "../components/SubmitToolForm";
+
+const SubmitPage = () => {
+  return (
+    <div className="min-h-screen px-4 pt-20 bg-zinc-50 dark:bg-zinc-950">
+      <SubmitToolForm />
+    </div>
+  );
+};
+
+export default SubmitPage;
